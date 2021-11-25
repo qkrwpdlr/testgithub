d8afbd682135abd530d6c3420329ca68bea00f9a
jake <wk10453@webtoonscorp.com>
2021-11-25 18:05:11 +0900

    refactor : 리듀스로 변경

--- index.js
+++ index.js
@@ -2,10 +2,4 @@ const numArr = []
 
 for (let i = 0; i < 10; i++)numArr.push(i + 1)
 
-const result = (() => {
-    let temp = 0
-    for (let i = 0; i < 10; i++) {
-        temp += numArr[i]
-    }
-    return temp
-})(numArr)
+const result = numArr.reduce((cur, pre) => cur + pre, 0)
